c2149ca4a73949a8d3da008d0fed81e35cdaff29
MarvinJWendt <github@marvinjwendt.com>
2019-01-02 04:23:11 +0100

    get from file and url

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 const terminalImage = require("terminal-image")
+import * as got from "got"
+
+/**
+ * Get string for terminal output from image file
+ * @param path Path to file
+ */
+export const fromFile = async (path: string) => {
+  return terminalImage.file(path)
+}
+
+/**
+ * Get string for terminal output from image url
+ * @param url URL of the image file
+ */
+export const fromURL = async (url: string) => {
+  const response = await got.get(url, { encoding: null })
+  return terminalImage.buffer(response.body)
+}
