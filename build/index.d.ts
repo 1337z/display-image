b4726f29292774c90126008666a7bc4754a40184
MarvinJWendt <github@marvinjwendt.com>
2019-01-02 05:10:47 +0100

    Minify

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,9 +2,9 @@
  * Get string for terminal output from image file
  * @param path Path to file
  */
-export declare const fromFile: (path: string) => Promise<any>
+export declare const fromFile: (path: string) => Promise<any>;
 /**
  * Get string for terminal output from image url
  * @param url URL of the image file
  */
-export declare const fromURL: (url: string) => Promise<any>
+export declare const fromURL: (url: string) => Promise<any>;
